908f6578927a17e87e23bb318526b071e4a6f9ef
余超 <yuzai20833>
2021-01-01 23:37:35 +0800

    提交

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <HelloWorld msg="Welcome to Your Vue.js App">
+      <template v-slot:about>
+        <About></About>
+      </template>
+      <template v-slot:h1>
+        <h1>h1插槽</h1>
+      </template>
+      <h2>h2 未被包裹的元素h2</h2>
+    </HelloWorld>
   </div>
 </template>
-
+s
 <script>
-// @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import About from '@/views/About.vue'
 
 export default {
   name: 'Home',
+  data(){
+    return {
+      date:new Date()
+    }
+  },
   components: {
-    HelloWorld
+    HelloWorld,
+    About
   }
 }
 </script>
